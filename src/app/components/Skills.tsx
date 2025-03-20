3216ffd1c35d61fa2656c7311d42b0cc33464c73
Vishnu Kothakapu <vishnukothakapu27@gmail.com>
2025-03-20 20:03:06 +0530

    fix errors{

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -52,7 +52,7 @@ const Skills = () => {
     const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
 
     useEffect(() => {
-        const handleMouseMove = (e) => {
+        const handleMouseMove = (e:any) => {
             setCursorPos({ x: e.clientX, y: e.clientY });
         };
         window.addEventListener("mousemove", handleMouseMove);
