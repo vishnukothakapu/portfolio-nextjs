f335298a4094d13938a081bd03a109c4f962bce7
Vishnu Kothakapu <vishnukothakapu27@gmail.com>
2025-03-21 19:38:27 +0530

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 import { Poppins,Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner"
 import {ThemeProvider} from "@/providers/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
-  title: "Vishnu's Portfolio",
-  description: "Hello, I'm Vishnu Kothakapu",
+  title: "Vishnu Kothakapu | Portfolio",
+  description: "Showcasing my expertise in full-stack web development, React, Next.js, and modern technologies. Explore my projects, skills, and achievements.",
+    icons: {
+        icon: "/assets/favicon.png",
+    },
 };
 
 export default function RootLayout({
@@ -16,6 +20,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+    <head>
+        <link rel="icon" href="/assets/favicon.png" sizes="any" />
+        <Script strategy="afterInteractive" src={"https://www.googletagmanager.com/gtag/js?id=G-D8VS1JQ5Q2"}/>
+        <Script id="google-analytics" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-D8VS1JQ5Q2', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+    </head>
       <body
         className={`font-satoshi antialiased  dark:bg-[#13151A]`}
       >
